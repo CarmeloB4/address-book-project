f07302c280d7e8335a99068792a50a4243d788b5
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-10-06 10:39:39 +0200

    configure module and routing

--- address-book/src/app/modules/address-book/address-book.module.ts
+++ address-book/src/app/modules/address-book/address-book.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddressBookComponent } from './pages/address-book/address-book.component';
+import { AddressDetailComponent } from './pages/address-detail/address-detail.component';
+import {AddressRoutingModule} from "./address-routing.module";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddressBookComponent,
+    AddressDetailComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AddressRoutingModule
   ]
 })
 export class AddressBookModule { }
