6be1cbdeb9c70fe27c4c7ecfaa2decf4db87664c
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-10-07 10:57:33 +0200

    create mocked data

---
+++ address-book/src/app/shared/mock-data/mock-contacts.ts
@@ -0,0 +1,39 @@
+import { Contact } from '../models/contact.model';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    firstname: 'Mario',
+    lastname: 'Rossi',
+    telephone: '3214545478',
+    email: 'mariorossi@mock.it',
+  },
+  {
+    id: 2,
+    firstname: 'Luca',
+    lastname: 'Bianchi',
+    telephone: '3217878789',
+    email: 'lucabianchi@mock.it',
+  },
+  {
+    id: 3,
+    firstname: 'Giovanni',
+    lastname: 'Rossi',
+    telephone: '3211212123',
+    email: 'giovannirossi@mock.it',
+  },
+  {
+    id: 4,
+    firstname: 'Francesco',
+    lastname: 'Verdi',
+    telephone: '3219696658',
+    email: 'fraverdi@mock.it',
+  },
+  {
+    id: 5,
+    firstname: 'Luigi',
+    lastname: 'Esposito',
+    telephone: '3215252525',
+    email: 'luigiesposito@mock.it',
+  },
+];
